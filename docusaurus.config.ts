4e0fc8ef7db3a9cdd3cbcf1d2c672ce8310f6176
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-06-10 12:52:39 -0400

    fix(config): docs link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,131 +1,131 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
-  title: "GX+",
-  tagline: "GX+, A FleetPoint Product",
-  favicon: "img/gxplus.png",
+	title: "GX+",
+	tagline: "GX+, A FleetPoint Product",
+	favicon: "img/gxplus.png",
 
-  // Set the production url of your site here
-  url: "https://fleetpoint.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/gxplus-docs/",
+	// Set the production url of your site here
+	url: "https://fleetpoint.github.io",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/gxplus-docs/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "FleetPoint", // Usually your GitHub org/user name.
-  projectName: "gxplus-docs", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "FleetPoint", // Usually your GitHub org/user name.
+	projectName: "gxplus-docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/fleetpoint/gxplus-docs/tree/master/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/fleetpoint/gxplus-docs/tree/master/",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/fleetpoint/gxplus-docs/tree/master/",
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/fleetpoint/gxplus-docs/tree/master/",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "GX+",
-      logo: {
-        alt: 'GX+ "Plus" Icon',
-        src: "img/gxplus.png",
-      },
-      items: [
-        {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Docs",
-        },
-        { to: "/blog", label: "Blog", position: "left" },
-        {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Docs",
-              to: "/docs/intro",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} GX+, a FleetPoint Product. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		// Replace with your project's social card
+		image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "GX+",
+			logo: {
+				alt: 'GX+ "Plus" Icon',
+				src: "img/gxplus.png",
+			},
+			items: [
+				{
+					type: "docSidebar",
+					sidebarId: "tutorialSidebar",
+					position: "left",
+					label: "Docs",
+				},
+				{ to: "/blog", label: "Blog", position: "left" },
+				{
+					href: "https://github.com/facebook/docusaurus",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Docs",
+					items: [
+						{
+							label: "Docs",
+							to: "/docs",
+						},
+					],
+				},
+				{
+					title: "Community",
+					items: [
+						{
+							label: "Stack Overflow",
+							href: "https://stackoverflow.com/questions/tagged/docusaurus",
+						},
+						{
+							label: "Discord",
+							href: "https://discordapp.com/invite/docusaurus",
+						},
+						{
+							label: "Twitter",
+							href: "https://twitter.com/docusaurus",
+						},
+					],
+				},
+				{
+					title: "More",
+					items: [
+						{
+							label: "Blog",
+							to: "/blog",
+						},
+						{
+							label: "GitHub",
+							href: "https://github.com/facebook/docusaurus",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} GX+, a FleetPoint Product. Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
